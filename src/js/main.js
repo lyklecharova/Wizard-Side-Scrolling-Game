26eb13f1c9a153ffb85596df20dae616b5e900d6
lyklecharova <lklecharova9@gmail.com>
2024-06-09 11:30:05 +0300

    Separate logic into different files

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-const startScreen = document.querySelector('.start-screen');
-const gameScreen = document.querySelector('.game-screen');
+let state = initState();
+let game = initGameObject();
 
-startScreen.addEventListener('click', (e) => {
-    e.currentTarget.classList.add('hidden');
-    gameScreen.classList.remove('hidden');
+game.startScreen.addEventListener('click', (e) => {
+    game.startScreen.classList.add('hidden');
+    game.gameScreen.classList.remove('hidden');
 
 });
