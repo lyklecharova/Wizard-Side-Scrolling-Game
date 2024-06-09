9039f96687ac70d53e237488ff013088a0e6bf8f
lyklecharova <lklecharova9@gmail.com>
2024-06-09 11:13:50 +0300

    Add empty start screen

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+const startScreen = document.querySelector('.start-screen');
+const gameScreen = document.querySelector('.game-screen');
+
+startScreen.addEventListener('click', (e) => {
+    e.currentTarget.classList.add('hidden');
+    gameScreen.classList.remove('hidden');
+
+});
